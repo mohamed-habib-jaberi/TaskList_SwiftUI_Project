04802413d9d10c3cd75d1b0b04a82f2de855a7a1
mohamed  habib <mohamedhabibjaberi111@gmail.com>
2020-06-08 17:22:36 +0200

    Add Data Models

---
+++ TaskList/Models/Task.swift
@@ -0,0 +1,12 @@
+//
+//  Task.swift
+//  TaskList
+//
+//  Created by mohamed  habib on 08/06/2020.
+//  Copyright © 2020 mohamed  habib. All rights reserved.
+//
+
+struct Task {
+    var name: String
+    var completed = false
+}
---
+++ TaskList/Models/TaskStore.swift
@@ -0,0 +1,11 @@
+//
+//  TaskStore.swift
+//  TaskList
+//
+//  Created by mohamed  habib on 08/06/2020.
+//  Copyright © 2020 mohamed  habib. All rights reserved.
+//
+
+class TaskStore {
+    
+}
