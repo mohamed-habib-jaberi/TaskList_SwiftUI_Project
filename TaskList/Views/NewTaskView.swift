c3d7337dadf7357b03a5a2f56fd13c94c3bd880a
mohamed  habib <mohamedhabibjaberi111@gmail.com>
2020-06-09 12:56:23 +0200

    Add new view

---
+++ TaskList/Views/NewTaskView.swift
@@ -0,0 +1,25 @@
+//
+//  NewTaskView.swift
+//  TaskList
+//
+//  Created by mohamed  habib on 09/06/2020.
+//  Copyright © 2020 mohamed  habib. All rights reserved.
+//
+
+import SwiftUI
+
+struct NewTaskView: View {
+    
+    @State var text = ""
+    
+    var body: some View {
+
+        TextField("Task Name", text: $text)
+    }
+}
+
+struct NewTaskView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewTaskView()
+    }
+}
